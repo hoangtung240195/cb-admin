149c023e1bc5e5d08839c2aee321334efc622bb6
Hoàng Tùng <hoangtung@Hoang-Tung-2.net.fpt>
2019-05-18 15:02:10 +0700

    Chinh lai menu va doi trang bao cao tong hop

--- srtdash/assets/js/line-chart.js
+++ srtdash/assets/js/line-chart.js
@@ -174,7 +174,7 @@ if ($('#verview-shart').length) {
         "type": "line",
 
         "scale-x": { //X-Axis
-            "labels": ["0", "10", "20", "30", "40", "50", "60", "70", "80", "90", "100"],
+            "labels": ["1/5", "2/5", "3/5", "4/5", "5/5", "6/5", "7/5"],
             "label": {
                 "font-size": 14,
                 "offset-x": 0,
@@ -190,7 +190,7 @@ if ($('#verview-shart').length) {
         },
         "plot": { "aspect": "spline" },
         "series": [{
-                "values": [20, 25, 30, 35, 45, 40, 40, 35, 25, 17, 40, 50],
+                "values": [20, 25, 30, 35, 45, 40, 35],
                 "line-color": "#F0B41A",
                 /* "dotted" | "dashed" */
                 "line-width": 5 /* in pixels */ ,
@@ -203,20 +203,20 @@ if ($('#verview-shart').length) {
                     /* hexadecimal or RBG value */
                 }
             },
-            {
-                "values": [40, 45, 30, 20, 30, 35, 45, 55, 40, 30, 55, 30],
-                "line-color": "#0884D9",
-                /* "dotted" | "dashed" */
-                "line-width": 5 /* in pixels */ ,
-                "marker": { /* Marker object */
-                    "background-color": "#067dce",
-                    /* hexadecimal or RGB value */
-                    "size": 5,
-                    /* in pixels */
-                    "border-color": "#067dce",
-                    /* hexadecimal or RBG value */
-                }
-            }
+            // {
+            //     "values": [40, 45, 30, 20, 30, 35, 45, 55, 40, 30, 55, 30],
+            //     "line-color": "#0884D9",
+            //     /* "dotted" | "dashed" */
+            //     "line-width": 5 /* in pixels */ ,
+            //     "marker": { /* Marker object */
+            //         "background-color": "#067dce",
+            //         /* hexadecimal or RGB value */
+            //         "size": 5,
+            //         /* in pixels */
+            //         "border-color": "#067dce",
+            //         /* hexadecimal or RBG value */
+            //     }
+            // }
         ]
     };
 
